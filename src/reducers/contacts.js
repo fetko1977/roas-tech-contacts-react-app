aba82bc29bf911f9643506dad33a1ba0fd21235d
Svetoslav Savov <svetoslav.savov@stanga.net>
2020-01-12 15:30:00 +0200

    console log debug axios request in heroku

--- src/reducers/contacts.js
+++ src/reducers/contacts.js
@@ -3,7 +3,7 @@ const contactsReducerDefautState = [];
 export default (state = contactsReducerDefautState, action) => {
     switch (action.type) {
         case 'GET_CONTACTS':
-            return [...state, action.contacts];
+            return action.contacts;
         default:
             return state;
     }
